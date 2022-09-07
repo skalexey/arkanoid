9a3d93ad66bc535b3c0d0e88b56fc9f13679c4fd
Alex <alexeiskorokhodov@gmail.com>
2022-09-07 16:50:23 +0300

    Update deps variables.

--- external_config.sh
+++ external_config.sh
@@ -4,8 +4,6 @@ source $THIS_DIR/os.sh
 
 if is_windows; then
 	export arkanoid_deps="${HOME}/Projects"
-	export build_libs="${HOME}/libs"
 else
-	export Arkanoid_deps="${HOME}/Projects"
-	export build_libs="${HOME}/libs"
+	export arkanoid_deps="${HOME}/Projects"
 fi
