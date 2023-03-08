676d5517f89a6c534e09ad1b2f8ecef4b3100d90
skalexey <skorohodov-ad@narod.ru>
2023-03-08 16:02:49 +0100

    Use local variables.

--- external_config.sh
+++ external_config.sh
@@ -1,11 +1,16 @@
 
-THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-source $THIS_DIR/os.sh
+function job()
+{
+	local THIS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+	source $THIS_DIR/os.sh
 
-if is_windows; then
-	export arkanoid_deps="${HOME}/Projects"
-else
-	export arkanoid_deps="${HOME}/Projects"
-fi
+	if is_windows; then
+		export arkanoid_deps="${HOME}/Projects"
+	else
+		export arkanoid_deps="${HOME}/Projects"
+	fi
 
-[ ! -z arkanoid_deps ] && build_deps=$arkanoid_deps
+	[ ! -z arkanoid_deps ] && build_deps=$arkanoid_deps
+}
+
+job $@
