84a110f266315bfb01da98afd442606d25e062d7
skalexey <skorohodov-ad@narod.ru>
2023-08-29 07:46:09 +0200

    Use tabs.

--- src/Utils.h
+++ src/Utils.h
@@ -2,7 +2,8 @@
 
 namespace Utils
 {
-    int random(int from, int to);
+	int random(int from, int to);
+	
 	template <typename T>
 	const T clamp(const T& v, const T& lo, const T& hi) {
 		return v < lo ? lo : hi < v ? hi : v;
